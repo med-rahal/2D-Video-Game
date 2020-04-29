e1486b43850a80f1f6ed3d821cf815ad0d3872d4
mariem-dev <60842684+mariem-dev@users.noreply.github.com>
2020-04-29 19:15:45 +0200

    Add files via upload

---
+++ personnage.h
@@ -0,0 +1,10 @@
+#ifndef ENEMY_H_INCLUDED
+#define ENEMY_H_INCLUDED
+#include<stdio.h>
+#include<stdlib.h>
+#include<SDL/SDL.h>
+#include <SDL/SDL_image.h>
+void setrects(SDL_Rect* clip);
+
+
+#endif
