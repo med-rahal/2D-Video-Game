8d674a37cd0e98780f8088eb9422a5245f269d6a
•²™♥MΣdit♥™²• <62762451+medit-F13@users.noreply.github.com>
2020-06-03 15:28:05 +0200

    Add files via upload

---
+++ barkia mohamed/CollisionBB/collisionbb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include "collisionbb.h"
+
+int collisionbb( SDL_Rect posj , SDL_Rect posobj )
+{
+ int x ; 
+if ( ((posj.y+posj.h>=posobj.y)&&(posj.y<=posobj.y+posobj.h)&&(posj.x+posj.w>=posobj.x)&&(posj.x<=posobj.x+posobj.w)) )
+{
+	x=1 ; 
+}
+
+else x=0 ; 
+return x ;
+}
+
+
---
+++ barkia mohamed/CollisionBB/collisionbb.h
@@ -0,0 +1,15 @@
+#ifndef FONCTIONS_H_
+#define FONCTIONS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+
+int collisionbb( SDL_Rect posj , SDL_Rect posobj ) ; 
+
+
+
+
+#endif /* FONCTIONS_H_ */
